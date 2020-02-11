dad1e6a2bd3ba86f66a0dbbeab3e64fa84c3051f
calmesam01 <sanyamsareen01@gmail.com>
2020-02-10 22:31:07 -0500

    Add GET and POST routes to the app

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function(req, res) {
+    Article.find(function(err, articles) {
+        res.send(articles);
+    });
+});
+
+app.post("/articles", function(req, res) {
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save(function(err) {
+        if (!err) {
+            res.send("Success");            
+        }
+        else {
+            res.send("Error!!");
+        }
+    });
+});
+
+app.listen(3000, function(err) {
+    if (err) {
+        console.log("Error!");
+    }
+    else {
+        console.log("Server is up and running on port 3000");
+    }
+})
